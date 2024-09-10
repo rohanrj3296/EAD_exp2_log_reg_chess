9b6913d8043f06705da8b00ea1ffcbdacc893b70
rohanrj3296 <147172259+rohanrj3296@users.noreply.github.com>
2024-09-10 14:44:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    if (display.innerText === '0') {
+        display.innerText = value;
+    } else {
+        display.innerText += value;
+    }
+}
+
+function clearDisplay() {
+    document.getElementById('display').innerText = '0';
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        
+        display.innerText = eval(display.innerText) || '0';
+    } catch {
+
+        display.innerText = 'Error';
+    }
+}
